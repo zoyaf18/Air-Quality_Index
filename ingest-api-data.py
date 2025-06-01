553fc5626de5678b0fb12ac618687743c5c9d6dc
Zoya Fatima <64043575+zoyaf18@users.noreply.github.com>
2025-06-02 04:48:51 +0530

    Update ingest-api-data.py

--- ingest-api-data.py
+++ ingest-api-data.py
@@ -26,7 +26,7 @@ today_string = current_time_ist.strftime('%Y_%m_%d')
 # Following credential has to come using secret whie running in automated way
 def snowpark_basic_auth() -> Session:
     connection_parameters = {
-       "ACCOUNT":"brmxyfgzq72391"",
+       "ACCOUNT":"brmxyfgzq72391",
        "region":"me-central-2",
         "USER":"zfatima",
         "PASSWORD":"zkgkEhc5xE2uL24",
